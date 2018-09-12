f11aee1b348e5ecd117abc29beed8406c1b11512
Harald Nezbeda <hn@anexia.at>
2018-09-12 18:30:49 +0200

    ticket-247523: Add build pipelines

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+    production: true,
+    BUILD_VERSION: 'ToDo: Version',
+    BUILD_DATE: 'ToDo: Date',
+    BUILD_DEVELOPMENT: false,
+    BUILD_PRODUCTION: true,
+    BUILD_TEST: false,
 };
