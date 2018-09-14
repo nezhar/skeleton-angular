bdf2bac08bbdbab1c9db369d0fc929a31b5a5e40
Harald Nezbeda <hn@anexia.at>
2018-09-14 17:21:33 +0200

    ticket-247523: Update translations

--- src/app/modules/ngx-anx-loading-screen/ngx-anx-loading-screen.module.ts
+++ src/app/modules/ngx-anx-loading-screen/ngx-anx-loading-screen.module.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, ModuleWithProviders, NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
-import { CommonModule } from "@angular/common";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 import { LoadingScreenService } from './loading-screen.service';
