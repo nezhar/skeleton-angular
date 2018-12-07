ff2e0c5ab7e9fb0d155b44c952f72dea2748856c
Harald Nezbeda <hn@anexia.at>
2018-12-07 11:19:10 +0100

    ticket-247523: Adapt storage engine to use defined key list

--- {{ cookiecutter.project_slug }}/src/app/shared/storage/app.storage.ts
+++ {{ cookiecutter.project_slug }}/src/app/shared/storage/app.storage.ts
@@ -1,7 +1,13 @@
 import { StorageEngine, STORAGE_ENGINE } from '@ngxs/storage-plugin';
+import * as _ from 'lodash';
 
 export class AppStorageEngine implements StorageEngine {
     length: number;
+    storageKeys = [
+        'auth.token',
+        'language',
+        'count',
+    ];
 
     getItem(key) {
         return window.localStorage.getItem(key);
@@ -9,15 +15,18 @@ export class AppStorageEngine implements StorageEngine {
 
     setItem(key, value) {
         const state = JSON.parse(value);
-        state.auth.user = null;
-        state.auth.loaded = false;
-        return window.localStorage.setItem(key, JSON.stringify(state));
+        const storage = {};
+        for (const storeKey of this.storageKeys) {
+            _.set(storage, storeKey, _.get(state, storeKey));
+        }
+        return window.localStorage.setItem(key, JSON.stringify(storage));
     }
 
     removeItem(key) {
         return window.localStorage.removeItem(key);
     }
 
+    // tslint:disable-next-line
     key(val: number) {
         console.log(val);
         return 'app.storage';
@@ -29,5 +38,5 @@ export class AppStorageEngine implements StorageEngine {
 
 export let appStorageProvider =  {
     provide: STORAGE_ENGINE,
-    useClass: AppStorageEngine
+    useClass: AppStorageEngine,
 };
